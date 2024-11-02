011d860d4ee40c847533787fbb3c8ded6e7fa68f
kelennison <121885421+kelennison@users.noreply.github.com>
2024-11-02 22:15:30 +0000

    Update visualization1.R

--- visualization1.R
+++ visualization1.R
@@ -1,12 +1,21 @@
-if (!require("BiocManager", quietly = TRUE)) {
+# Install BiocManager if it isn't already installed
+if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
-}
 
-# Set Bioconductor to version 3.20
+# Set the Bioconductor version to 3.20
 BiocManager::install(version = "3.20")
 
-# Install ggtree with force to ensure it overwrites any partial installations
+# Force install ggtree from Bioconductor
 BiocManager::install("ggtree", force = TRUE)
+
+# Force install tidyverse from CRAN if not already installed
+if (!require("tidyverse", quietly = TRUE))
+    install.packages("tidyverse", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+
+# Force install ape from CRAN if not already installed
+if (!require("ape", quietly = TRUE))
+    install.packages("ape", repos = "http://cran.us.r-project.org", dependencies = TRUE)
+
 
 
 # Load necessary libraries
