ad950d17dab700177567583541dc2a047374760d
kelennison <121885421+kelennison@users.noreply.github.com>
2024-11-02 14:24:22 +0000

    Update Home.py

--- Home.py
+++ Home.py
@@ -125,24 +125,29 @@ if st.button('Run Visualization'):
     # Print current working directory for debugging
     st.write("Current Working Directory:", os.getcwd())
 
-    result_viz1 = subprocess.run(["Rscript", "visualization1.R"], capture_output=True, text=True)
-    if result_viz1.returncode == 0:
+    # Run visualization1.R with subprocess.Popen
+    process_viz1 = subprocess.Popen(["Rscript", "visualization1.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout_viz1, stderr_viz1 = process_viz1.communicate()
+
+    if process_viz1.returncode == 0:
         st.success("Visualization 1 completed!")
         image = Image.open("viz1.png")  # Load the generated image
         st.image(image, caption="Visualization 1", width=1000)
     else:
         st.error("Error in Visualization 1.")
-        st.write(result_viz1.stderr)
+        st.write(stderr_viz1)
 
-    
-    result_viz2 = subprocess.run(["Rscript", "visualization2.R"], capture_output=True, text=True)
-    if result_viz2.returncode == 0:
+    # Run visualization2.R with subprocess.Popen
+    process_viz2 = subprocess.Popen(["Rscript", "visualization2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout_viz2, stderr_viz2 = process_viz2.communicate()
+
+    if process_viz2.returncode == 0:
         st.success("Visualization 2 completed!")
         image = Image.open("viz2.png")  # Load the generated image
         st.image(image, caption="Visualization 2", width=1000)
     else:
         st.error("Error in Visualization 2.")
-        st.write(result_viz2.stderr)
+        st.write(stderr_viz2)
 
 
    
