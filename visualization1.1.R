0e20a92e4311232e3b4a59e6b4980ac11ee80027
Your Namekelennison <you@example.com>
2024-12-22 23:34:07 +0000

    description of changes

--- visualization1.1.R
+++ visualization1.1.R
@@ -1,22 +1,3 @@
-# Install BiocManager if it isn't already installed
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-# Set the Bioconductor version to 3.20
-BiocManager::install(version = "3.20")
-
-# Force install ggtree from Bioconductor
-BiocManager::install("ggtree", force = TRUE)
-
-# Force install tidyverse from CRAN if not already installed
-if (!require("tidyverse", quietly = TRUE))
-    install.packages("tidyverse", repos = "http://cran.us.r-project.org", dependencies = TRUE)
-
-# Force install ape from CRAN if not already installed
-if (!require("ape", quietly = TRUE))
-    install.packages("ape", repos = "http://cran.us.r-project.org", dependencies = TRUE
-
-
 # Load required packages
 library(tidyverse)
 library(ape)
