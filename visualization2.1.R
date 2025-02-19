ad33a73fc0c692a755e2ef75f2d802fba70d282d
Your Namekelennison <you@example.com>
2025-02-19 20:49:46 +0000

    description of changes

--- visualization2.1.R
+++ visualization2.1.R
@@ -308,5 +308,5 @@ tree_plot_new <- tree_plot_new +
   geom_tree(color = "black", size = 1) +  # Make entire tree black
   scale_color_identity()  # Use specified colors directly
 
-print(tree_plot)
-ggsave("images/viz2.png", plot = tree_plot, width = 12, height = 8, dpi = 300)
+print(tree_plot_new)
+ggsave("images/viz2.png", plot = tree_plot_new, width = 12, height = 8, dpi = 300)
